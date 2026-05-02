d97834bcd237f0764c743167e85ee217ade403f4
Faisal Majeed <faisal@example.com>
2026-05-02 11:54:33 -0700

    Final VIP Design and Settings Fix

--- instacart/settings.py
+++ instacart/settings.py
@@ -84,7 +84,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Ye line files ko compress karti hai taake site fast chale
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorag'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
